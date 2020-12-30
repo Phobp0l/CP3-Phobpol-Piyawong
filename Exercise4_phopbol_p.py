00c5e8529d9187614c9add194dfe7c6797df1af9
Phobp0l <76761882+Phobp0l@users.noreply.github.com>
2020-12-30 10:56:44 +0700

    Exercise4_phobpol_p.py

---
+++ Exercise4_phopbol_p.py
@@ -0,0 +1,9 @@
+print(-----your score-----)
+FD=85.3
+GB=89.1
+ITCS=89.9
+CP=99.3
+print("Foundation English:",FD)
+print("General Business:",GB)
+print("Introduction to Computer Systems:",ITCS)
+print("Computer Programming:",CP)
